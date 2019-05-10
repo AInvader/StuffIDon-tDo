501cfc46c8e681929d4e38283fb741280dcba4d5
AInvader <36476225+AInvader@users.noreply.github.com>
2019-05-10 15:38:24 +0530

    Rt

--- src/ClockAngle.java
+++ src/ClockAngle.java
@@ -1,36 +1,38 @@
 import java.util.Scanner;
+
 class ClockAngle {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter Time : ");
-        String time=sc.nextLine();
-        int colon_pos=time.indexOf(':');
-        int hour=Integer.parseInt(time.substring(0,colon_pos));
-        int minute=Integer.parseInt(time.substring(colon_pos+1));
-        double ang=Angle(hour,minute);
-        System.out.print("Angle between Hour hand and Minute Hand = "+ang);
+        String time = sc.nextLine();
+        int colon_pos = time.indexOf(':');
+        int hour = Integer.parseInt(time.substring(0, colon_pos));
+        int minute = Integer.parseInt(time.substring(colon_pos + 1));
+        double ang = Angle(hour, minute);
+        System.out.print("Angle between Hour hand and Minute Hand = " + ang);
     }
-    private static double Angle (int hour, int minute) {
-        if (hour>12)
-            hour=hour-12;
-        if (hour==0)
-            hour=12;
-        System.out.println("Hours = "+hour+"\nMinutes = "+minute);
+
+    private static double Angle(int hour, int minute) {
+        if (hour > 12)
+            hour = hour - 12;
+        if (hour == 0)
+            hour = 12;
+        System.out.println("Hours = " + hour + "\nMinutes = " + minute);
         double angle;
-        int minute_dev=6;   
-        int min5_dev=30;
-        double hour_hand_dev = (1.0/2.0)*(double)minute;   
-        if (hour==12) {         
-            angle=(double)(((minute/5)*30)+(minute%5)*minute_dev)-hour_hand_dev;           
+        int minute_dev = 6;
+        int min5_dev = 30;
+        double hour_hand_dev = (1.0 / 2.0) * (double) minute;
+        int i = ((minute / 5) * 30) + (minute % 5) * minute_dev;
+        if (hour == 12) {
+            angle = (double) i - hour_hand_dev;
+        } else {
+            double hour_hand_angle = (double) hour * 30;
+            angle = (double) i - hour_hand_angle - hour_hand_dev;
         }
-        else {
-            double hour_hand_angle=(double)hour*30;
-            angle=(double)(((minute/5)*30)+(minute%5)*minute_dev)-hour_hand_angle-hour_hand_dev;            
-        }        
-        if (angle>180.0)
-            angle=360-angle;
-        if (angle<0)
-            angle=angle*-1;  
+        if (angle > 180.0)
+            angle = 360 - angle;
+        if (angle < 0)
+            angle = angle * -1;
         return angle;
-    }        
+    }
 }
--- src/ConsSum.java
+++ src/ConsSum.java
@@ -1,24 +1,26 @@
 class ConsSum {
-	public static void main(String[] args) {
-		int num=33;
-		ConsSumPrinter(num);
-	}
-	private static void ConsSumPrinter(int num) {
-		int[] vals =new int[2];
-		for (int n=1;n<num;n++) {
-			for (int a=1;a<num;a++) {
-				if ((n/2)*(2*a+(n-1))==num) {
-					vals[0]=a;
-					vals[1]=n;
-					break;
-				}
-			}
-		}
-		SeriesPrinter(vals);
-	}
-	private static void SeriesPrinter(int[] vals) {
-		for (int i=1;i<=vals[1];i++) {
-			System.out.print(vals[0]+(i-1)+"  ");
-		}
-	}
+    public static void main(String[] args) {
+        int num = 33;
+        ConsSumPrinter(num);
+    }
+
+    private static void ConsSumPrinter(int num) {
+        int[] vals = new int[2];
+        for (int n = 1; n < num; n++) {
+            for (int a = 1; a < num; a++) {
+                if ((n / 2) * (2 * a + (n - 1)) == num) {
+                    vals[0] = a;
+                    vals[1] = n;
+                    break;
+                }
+            }
+        }
+        SeriesPrinter(vals);
+    }
+
+    private static void SeriesPrinter(int[] vals) {
+        for (int i = 1; i <= vals[1]; i++) {
+            System.out.print(vals[0] + (i - 1) + "  ");
+        }
+    }
 }
